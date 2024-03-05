6bb497d36c972ac38eabc224c2fad97ce30d627c
LCBRST <50761136+LCBRST@users.noreply.github.com>
2024-03-05 07:32:52 +0000

    Pending changes exported from your codespace

---
+++ ch02/JavaApp2_2.java
@@ -0,0 +1,13 @@
+package ch02;
+import java.util.Scanner;
+public class JavaApp2_2 {
+    public static void main(String[] args){
+        int i=0;
+        float f=0.0f;
+        //创建Scanner类对象 完成从键盘输入操作
+        Scanner scan=new Scanner(System.in);
+        //从键盘输入一个整数
+        if(scan.hasNextInt()) i=scan.nextInt();
+        System.out.println("您输入的整数是："+i);
+    }
+}
