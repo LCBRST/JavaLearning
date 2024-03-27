e20ff2481c7d0680aaa7d8a7bc47070e2939c16c
LCBRST <50761136+LCBRST@users.noreply.github.com>
2024-03-27 11:47:21 +0000

    modified:   demo/src/ch05/JavaApp5_8.java

--- demo/src/ch05/JavaApp5_8.java
+++ demo/src/ch05/JavaApp5_8.java
@@ -1,19 +1,24 @@
 package ch05;
 
+import java.util.Scanner;
+
 public class JavaApp5_8 {
     public static void main(String[] args) {
         // 判断回文字符串
         // 构建原字符串的逆序串
-        String str="anc";
+        String str="";
         String newStr="";
         String sub="";
-
+        Scanner sc=new Scanner(System.in);
+        str=sc.nextLine();
         for(int i=0;i<str.length();i++){
             sub=str.substring(i, i+1);
             newStr=sub+newStr;
         }
-        if(newStr.equals(sub)){
-            System.out.println("该串是回文串");
+        if(newStr.equals(str)){
+            System.out.println("True");
+        }else{
+            System.out.println("False");
         }
     }
 }
