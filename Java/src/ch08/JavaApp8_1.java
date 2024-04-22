80a79c34232545554b788517ec4620f5b411b1b8
LCBRST <50761136+LCBRST@users.noreply.github.com>
2024-04-22 03:40:16 +0000

    new file:   Java/src/ch08/JavaApp8_1.java

---
+++ Java/src/ch08/JavaApp8_1.java
@@ -0,0 +1,12 @@
+package ch08;
+
+public class JavaApp8_1 {
+    public static void main(String[] args) {
+        //输出一个数组的所有元素
+        int[] a={1,2,3,4,5};
+        for(int i=0;i<=5;i++){
+            System.out.println("a["+i+"]="+a[i]);
+        }
+            System.out.println("5/0="+5/0);
+    }
+}
