ee5f1870f39723842f4fc1fe10d9516f1fe0cc00
LCBRST <50761136+LCBRST@users.noreply.github.com>
2024-04-22 03:14:56 +0000

    new file:   Java/src/ch07/myDate.java

---
+++ Java/src/ch07/myDate.java
@@ -0,0 +1,9 @@
+package ch07;
+import java.util.Date;
+public class myDate {
+    public static void main(String[] args) {
+        Date curruntDate=new Date();
+        System.out.println(curruntDate);
+    }
+    
+}
