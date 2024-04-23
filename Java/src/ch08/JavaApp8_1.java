82b02505d28917c17a5f254d7f4a2d8347309521
LCBRST <50761136+LCBRST@users.noreply.github.com>
2024-04-23 09:26:18 +0000

    modified:   Java/src/ch08/JavaApp8_1.java

--- Java/src/ch08/JavaApp8_1.java
+++ Java/src/ch08/JavaApp8_1.java
@@ -2,11 +2,25 @@ package ch08;
 
 public class JavaApp8_1 {
     public static void main(String[] args) {
+        // 异常处理
         //输出一个数组的所有元素
         int[] a={1,2,3,4,5};
+        int x=5,y=0;
         for(int i=0;i<=5;i++){
-            System.out.println("a["+i+"]="+a[i]);
+            try{
+                System.out.println("a["+i+"]="+a[i]);
+                System.out.println(x/y);
+            }
+            catch(ArrayIndexOutOfBoundsException e){
+                System.out.println("大傻逼 数组下标越界了 "+e.getMessage());
+            }
+            catch(ArithmeticException e){
+                System.out.println("大傻逼 你家除数能为0？ "+e.getMessage());
+            }
+            finally{
+                
+            }
         }
-            System.out.println("5/0="+5/0);
+            System.out.println("5/0");
     }
 }
