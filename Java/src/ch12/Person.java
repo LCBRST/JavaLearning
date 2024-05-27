6c73f567d73149a17d1500011c4f0b2a4e41a4c7
LCBRST <50761136+LCBRST@users.noreply.github.com>
2024-05-27 02:49:29 +0000

    反射

--- Java/src/ch12/Person.java
+++ Java/src/ch12/Person.java
@@ -23,8 +23,10 @@ public class Person {
     public void setAge(int age) {
         this.age = age;
     }
-    public String toString(){
-        return name;
+    
+    @Override
+    public String toString() {
+        return "Person [name=" + name + ", age=" + age + "]";
     }
     //笑感方法
     public void smile(){
--- Java/src/ch12/Test.java
+++ Java/src/ch12/Test.java
@@ -1,5 +1,8 @@
 package ch12;
 
+import java.lang.reflect.Constructor;
+
+@SuppressWarnings(value = { "all" })
 public class Test {
     public static void main(String[] args) throws Exception {
         // Person p1=new Person();
@@ -14,8 +17,13 @@ public class Test {
         p1.smile();
         //2.用类名调用class
         Class cls2=Person.class;
+        Constructor cobj=cls2.getConstructor(String.class,int.class);
+        Person p3=(Person) cobj.newInstance("张三",20);
         //通过对象调用getclass方法
         Person p2 =new Person();
         Class cls3=p2.getClass();
+
+        System.out.println(p3.toString());
+
     }
 }
